c37eb276ed20afee3b58a70dd842fa4cdf015ae3
StellaZuo <1404178033@qq.com>
2026-07-31 01:38:51 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE = 'stella-v2';
-const SHELL_SUFFIX = ['/', '/index.html', '/app.js', '/styles.css', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'stella-v3';
+const SHELL_SUFFIX = ['/', '/index.html', '/app.js', '/app-v4.js', '/app-v5.js', '/styles.css', '/styles-v4.css', '/styles-v5.css', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 const ASSETS = [
   './',
   './index.html',
   './app.js',
+  './app-v4.js',
+  './app-v5.js',
   './styles.css',
+  './styles-v4.css',
+  './styles-v5.css',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
